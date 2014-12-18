a2686d2928fe87739178c9ecb36e21de9e74b98e
jubianchi <julien.bianchi@pmsipilot.com>
2014-12-18 12:42:16 +0100

    Typo

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 node['user_ssh_keys']['users'].each do |username, user|
-  user_user_ssh_keys_key username do
+  user_ssh_keys_key username do
     authorized_keys user['authorized_keys']
     authorized_users user['authorized_users']
     data_bag node['user_ssh_keys']['data_bag']
