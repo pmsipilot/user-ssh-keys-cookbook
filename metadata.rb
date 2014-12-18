d882a75cf74e2ca1e3a31a438f2088dc7747e3ef
jubianchi <julien.bianchi@pmsipilot.com>
2014-12-18 10:12:26 +0100

    Bump to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'support@pmsipilot.com'
 license          'MIT'
 description      'Deploys SSH keys'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
